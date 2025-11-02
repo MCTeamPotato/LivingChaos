e7fb5174685b8956b32685a599fb1f4d69adb96b
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-11-02 21:17:41 +0800

    Update EntityMixin.java

--- src/main/java/me/kall/livingchaos/mixin/api/EntityMixin.java
+++ src/main/java/me/kall/livingchaos/mixin/api/EntityMixin.java
@@ -1,23 +1,33 @@
 package me.kall.livingchaos.mixin.api;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import me.kall.livingchaos.api.event.EntityChunkChangeEvent;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraftforge.common.MinecraftForge;
-import org.objectweb.asm.Opcodes;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
+    @Shadow public abstract ChunkPos chunkPosition();
+
     @Inject(method = "setPosRaw", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/ChunkPos;<init>(Lnet/minecraft/core/BlockPos;)V"))
     private void beforeChunkPosUpdate(CallbackInfo ci) {
         MinecraftForge.EVENT_BUS.post(new EntityChunkChangeEvent.Before((Entity) (Object) this));
     }
 
-    @Inject(method = "setPosRaw", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/Entity;chunkPosition:Lnet/minecraft/world/level/ChunkPos;", shift = At.Shift.AFTER, opcode = Opcodes.PUTFIELD))
-    private void afterChunkPosUpdate(CallbackInfo ci) {
+    @WrapMethod(method = "setPosRaw")
+    private void onChunkUpdate(double x, double y, double z, @NotNull Operation<Void> original) {
+        long before = this.chunkPosition().toLong();
+        original.call(x, y, z);
+        long after = this.chunkPosition().toLong();
+        if (before == after) return;
         MinecraftForge.EVENT_BUS.post(new EntityChunkChangeEvent.After((Entity) (Object) this));
     }
 }
